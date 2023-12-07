7048fd23871626cc49e05a7139d17464b179df75
Radi Galani <radi_d_1@yahoo.de>
2023-12-07 08:47:24 +0100

    my <<<<<<style

--- src/ColorButton.js
+++ src/ColorButton.js
@@ -7,7 +7,13 @@ import withColor from './withColor';
         return(
             <div>
                 <button style ={{color: this.props.color,
-                                 backgroundColor :this.props.backgroundColor}} 
+                                 backgroundColor :this.props.backgroundColor,
+                                 padding: "8px 16px",
+                                 borderRadius:
+                                 "4px",
+                                 fontSize:"18px",
+                                 fontWeight: "bold",
+                                 boxShadow: "0 0 4px rgba(0, 0, 0, 0.2)",}} 
                                  onClick={this.props.handleChangeColor} >
                     {this.props.buttonText}
                 </button>
